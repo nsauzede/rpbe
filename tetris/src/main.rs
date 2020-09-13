f899cfa70d834744ec8b2b8d72dc23163863043d
Nicolas Sauzede <nsauzede@laposte.net>
2020-09-13 17:17:13 +0200

    Add scores load/save

--- tetris/src/main.rs
+++ tetris/src/main.rs
@@ -12,6 +12,9 @@ use sdl2::image::{InitFlag, LoadTexture};
 use std::thread::sleep;
 use std::time::{Duration, SystemTime};
 
+use std::fs::File;
+use std::io::{self, Read, Write};
+
 const TEXTURE_SIZE: u32 = 32;
 
 #[derive(Clone, Copy)]
@@ -48,7 +51,60 @@ fn create_texture_rect<'a>(
     }
 }
 
-pub fn main() {
+fn write_into_file(content: &str, file_name: &str) -> io::Result<()> {
+    let mut f = File::create(file_name)?;
+    f.write_all(content.as_bytes())
+}
+
+fn read_from_file(file_name: &str) -> io::Result<String> {
+    let mut f = File::open(file_name)?;
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn slice_to_string(slice: &[u32]) -> String {
+    slice
+        .iter()
+        .map(|highscore| highscore.to_string())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn save_highscores_and_lines(highscores: &[u32], number_of_lines: &[u32]) -> bool {
+    let s_highscores = slice_to_string(highscores);
+    let s_number_of_lines = slice_to_string(number_of_lines);
+    write_into_file(
+        &format!("{}\n{}\n", s_highscores, s_number_of_lines),
+        "scores.txt",
+    )
+    .is_ok()
+}
+
+fn line_to_slice(line: &str) -> Vec<u32> {
+    line.split(" ")
+        .filter_map(|nb| nb.parse::<u32>().ok())
+        .collect()
+}
+
+fn load_highscores_and_lines() -> Option<(Vec<u32>, Vec<u32>)> {
+    if let Ok(content) = read_from_file("scores.txt") {
+        let mut lines = content
+            .splitn(2, "\n")
+            .map(|line| line_to_slice(line))
+            .collect::<Vec<_>>();
+        if lines.len() == 2 {
+            let (number_lines, highscores) = (lines.pop().unwrap(), lines.pop().unwrap());
+            Some((highscores, number_lines))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+fn main() {
     let sdl_context = sdl2::init().expect("SDL initialization failed");
 
     sdl2::image::init(InitFlag::PNG | InitFlag::JPG).expect(
