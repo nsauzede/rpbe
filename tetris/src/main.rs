0fc944e857e5b1727a636984dbb0be7090d2f9de
Nicolas Sauzede <nsauzede@laposte.net>
2020-09-13 12:07:55 +0200

    Add blinking rect

--- tetris/src/main.rs
+++ tetris/src/main.rs
@@ -4,12 +4,47 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
-use sdl2::render::{Texture, TextureCreator};
+use sdl2::render::{Canvas, Texture, TextureCreator};
+use sdl2::video::{Window, WindowContext};
 
 use std::thread::sleep;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 
 const TEXTURE_SIZE: u32 = 32;
+
+#[derive(Clone, Copy)]
+enum TextureColor {
+    Black,
+    White,
+    Red,
+    Green,
+    Blue,
+}
+
+fn create_texture_rect<'a>(
+    canvas: &mut Canvas<Window>,
+    texture_creator: &'a TextureCreator<WindowContext>,
+    color: TextureColor,
+    size: u32,
+) -> Option<Texture<'a>> {
+    if let Ok(mut square_texture) = texture_creator.create_texture_target(None, size, size) {
+        canvas
+            .with_texture_canvas(&mut square_texture, |texture| {
+                match color {
+                    TextureColor::Black => texture.set_draw_color(Color::RGB(0, 0, 0)),
+                    TextureColor::White => texture.set_draw_color(Color::RGB(255, 255, 255)),
+                    TextureColor::Red => texture.set_draw_color(Color::RGB(255, 0, 0)),
+                    TextureColor::Green => texture.set_draw_color(Color::RGB(0, 255, 0)),
+                    TextureColor::Blue => texture.set_draw_color(Color::RGB(0, 0, 255)),
+                }
+                texture.clear();
+            })
+            .expect("Failed to color a texture");
+        Some(square_texture)
+    } else {
+        None
+    }
+}
 
 pub fn main() {
     let sdl_context = sdl2::init().expect("SDL initialization failed");
@@ -32,20 +67,11 @@ pub fn main() {
         .expect("Failed to convert window into canvas");
 
     let texture_creator: TextureCreator<_> = canvas.texture_creator();
-    let mut square_texture: Texture = texture_creator
-        .create_texture_target(None, TEXTURE_SIZE, TEXTURE_SIZE)
-        .expect("Failed to create a texture");
-    canvas
-        .with_texture_canvas(&mut square_texture, |texture| {
-            texture.set_draw_color(Color::RGB(0, 255, 0));
-            texture.clear();
-        })
-        .expect("Failed to color a texture");
-
     let mut event_pump = sdl_context
         .event_pump()
         .expect("Failed to get SDL event pump");
 
+    let now = SystemTime::now();
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -60,6 +86,20 @@ pub fn main() {
 
         canvas.set_draw_color(Color::RGB(255, 0, 0));
         canvas.clear();
+
+        let col = if let Ok(elapsed) = now.elapsed() {
+            //println!("elapsed={}", elapsed.as_secs());
+            if elapsed.as_secs() % 2 == 0 {
+                TextureColor::Green
+            } else {
+                TextureColor::Blue
+            }
+        } else {
+            TextureColor::Black
+        };
+        let square_texture = create_texture_rect(&mut canvas, &texture_creator, col, TEXTURE_SIZE)
+            .expect("Failed to create a texture");
+
         canvas
             .copy(
                 &square_texture,
